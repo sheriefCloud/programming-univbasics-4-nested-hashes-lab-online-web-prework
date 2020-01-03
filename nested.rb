f27e0067b4012ab45f49345539dc9e93999ca5ad
Sherief Farid <sherief@farid.cloud>
2020-01-03 00:03:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchies][:languages].join
+		 dennis_ritchies_language = programmer_hash[:dennis_ritchies][:languages].join
 
 end
 
