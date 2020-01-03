8e1f5ea27ca945205cb509c80e4a361f935fc79c
Sherief Farid <sherief@farid.cloud>
2020-01-03 00:32:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,6 +61,8 @@ def dennis_ritchies_language
 			 to_txt = programmer_hash[:dennis_ritchies][:languages]
 
 			 programmer_hash[:dennis_ritchies][:dennis_ritchies_language] = to_txt.join
+
+			 programmer_hash
 		 end
 
 
